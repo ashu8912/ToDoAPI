281d7c1f980d4223b9baed244f512742cacb042d
AlwaysConfused9 <alwaysconfused7@gmail.com>
2018-07-08 19:49:37 +0530

    hashed user password before saving to database

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,13 +2,24 @@ const {SHA256}=require('crypto-js');
 var password="Ashu@123";
 var hash=SHA256(password).toString();
 const jwt=require('jsonwebtoken');
-var data={
-    id:4
-}
-var token=jwt.sign(data,"ashu@123");
-var decoder=jwt.verify(token,"ashu@123");
-console.log(token);
-console.log(decoder);
+const bcrypt=require('bcryptjs');
+var password="ashu@123";
+bcrypt.genSalt(10,(err,saltValue)=>{
+    bcrypt.hash(password,saltValue,(err,hash)=>{
+console.log(hash);
+    })
+});
+var hashedPassword='$2a$10$I50Y6CEoEMgkrge7ku2rOeMMJeK65O1d2QzDrMQv0F3gk9K8FbdF2'
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+console.log(res);
+})
+// var data={
+//     id:4
+// }
+// var token=jwt.sign(data,"ashu@123");
+// var decoder=jwt.verify(token,"ashu@123");
+// console.log(token);
+// console.log(decoder);
 // var token={
 //     data,
 //     hash:SHA256(JSON.stringify(data)+'someSecret').toString()
--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,7 @@ mongoose.Promise=global.Promise;
 var Schema=mongoose.Schema;
 var jwt=require('jsonwebtoken');
 var _=require('lodash');
+const bcrypt=require('bcryptjs');
 var userSchema=new Schema({
     email:{
         type:String,
@@ -65,6 +66,20 @@ var token=jwt.sign({_id:user._id,access},"abc123").toString();
 user.tokens.push({access,token});
 return user.save().then(()=>token);
 }
+userSchema.pre('save',function(next){
+var user=this;
+if(user.isModified('password')){
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password=hash;
+        next();
+    })
+})
+}
+else{
+    next();
+}
+})
 var User=mongoose.model('Users',userSchema);
 module.exports={
     User
